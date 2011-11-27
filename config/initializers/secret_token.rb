247e227580197835ffa31f6b36b1e8b08addc50b
Jonathan Abramson <zenpoy@gmail.com>
2011-11-27 17:24:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MeetingsApp::Application.config.secret_token = 'c9a8fcaa5fb3a18ee5e1f9b7c58e053ea132893b4af91beac69dd295892468e2b6c9807c23254035c615cae5f43d06aed516489638bb1947617127116c057124'
